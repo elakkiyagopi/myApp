bb2bdd706665b6b292062878fe0dc888dcf3f3da
elakkiyagopi <elakkiyagopi@anandtechverce.in>
2024-05-03 13:38:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemsComponent } from './recipe/recipe-list/recipe-items/recipe-items.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeItemsComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
